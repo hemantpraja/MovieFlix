d55e3466d2e88c0d84e87fb8a8cf5e6d1351c6b9
Hemant Prajapati <hemantpraja88@gmail.com>
2024-03-12 20:35:17 +0530

    comp 2.0

--- sorce code/src/app/components/Header.js
+++ sorce code/src/app/components/Header.js
@@ -4,12 +4,23 @@ import Image from 'next/image';
 import styles from "@/app/styles/navbar.module.css";
 import Nav from './Nav.js';
 
+
+const logo = {
+    border: '5px',
+    borderRadius: '10%',
+    borderStyle: 'solid',
+    borderColor: 'black',
+}
+
 const Header = (props) => {
     return (
         <header className={styles.main_header}>
             <div lcassname={styles.navbar_brand}>
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={150} height={40} />
+                    {/* <Image src="/logo.png" alt="logo" width={150} height={40} /> */}
+                    <div className={logo}>
+                        <h1>MovieFlix</h1>
+                    </div>
                 </Link>
 
             </div>
--- sorce code/src/app/components/Nav.js
+++ sorce code/src/app/components/Nav.js
@@ -1,9 +1,9 @@
 'use client'
 
 import styles from "@/app/styles/navbar.module.css"
-import {CgCloseR, CgMenu} from "react-icons/cg";
+import { CgCloseR, CgMenu } from "react-icons/cg";
 import Link from "next/link";
-import {useState} from "react";
+import { useState } from "react";
 
 const Nav = () => {
     const [openMenu, setOpenMenu] = useState(false);
@@ -11,27 +11,27 @@ const Nav = () => {
     return (
         <>
             <nav className={styles.navbar}>
-                <div className={openMenu ? `${styles.active}` : "" }>
+                <div className={openMenu ? `${styles.active}` : ""}>
                     <ul className={styles.navbarList}>
                         <li className={styles.navbarItem}>
-                            <Link className={styles.navbarLink} href="#"
-                                  onClick={() => setOpenMenu(false)}
+                            <Link className={styles.navbarLink} href="/"
+                                onClick={() => setOpenMenu(false)}
                             >Home</Link>
                         </li>
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink} href="/about"
-                                  onClick={() => setOpenMenu(false)}
+                                onClick={() => setOpenMenu(false)}
                             >About</Link>
                         </li>
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink}
-                                  onClick={() => setOpenMenu(false)}
-                                  href="/movie">Movie</Link>
+                                onClick={() => setOpenMenu(false)}
+                                href="/movies">Movie</Link>
                         </li>
                         <li className={styles.navbarItem}>
                             <Link className={styles.navbarLink}
-                                  onClick={() => setOpenMenu(false)}
-                                  href="/contact">Contact</Link>
+                                onClick={() => setOpenMenu(false)}
+                                href="/contact">Contact</Link>
                         </li>
                     </ul>
 
